cb35e0b098a5cf8bebec4921dc0fc8b6b77859ef
TwigDemonTF <91436428+TwigDemonTF@users.noreply.github.com>
2025-03-07 01:23:07 +0100

    update itemMonitor

--- itemMonitor.lua
+++ itemMonitor.lua
@@ -2,10 +2,11 @@ local meBridge = peripheral.find("meBridge")
 local checkInterval = 60 -- Seconds between checks
 local apiUrl = "http://your-flask-api-address/item/update"
 
--- Table of items to monitor
+-- Table of items to monitor with their target PCs
 local itemsToMonitor = {
-    { name = "thermal:phytogro" },
-    { name = "mekanism:bio_fuel" },
+    { name = "thermal:phytogro", target_pc = "autoCrafter" },
+    { name = "mekanism:bio_fuel", target_pc = "autoCrafter" },
+    { name = "botania:ender_air_bottle", target_pc = "enderAirTurtle" },
 }
 
 -- Function to get item count in ME system
@@ -22,8 +23,10 @@ local function sendData()
     local data = {}
     for _, itemData in ipairs(itemsToMonitor) do
         local itemName = itemData.name
+        local targetPc = itemData.target_pc
         local amount = getItemCount(itemName)
-        table.insert(data, { name = itemName, amount = amount })
+
+        table.insert(data, { name = itemName, amount = amount, target_pc = targetPc })
     end
 
     -- Convert data to JSON and send to API
